dcf00dd135579061adbddd65acade84eee0cc10f
nesterenkosasha <sasha.s.nesterenko@gmail.com>
2018-04-30 19:11:43 +0300

    Verstka

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const menuBarNav = document.getElementsByClassName("navigation-bar")[0]
 const subMenu = document.getElementsByClassName("sub-navigation-list")[0]
 
 
-
-
 function changeView({target}){
     target.style.display = "none"
     subMenu.style.display = "block"
